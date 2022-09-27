97e34c6694064f5496f2d169b3d8f04e8bca37f6
Jaden Lund <lundjaden@gmail.com>
2022-09-27 10:29:12 -0500

    commit tree 29ab2fa3b39e3a03aa5dced1e4e96bee093c58b4

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,11 @@
 class UsersController < ApplicationController
-
   def show
     user = User.find_by(id: params[:id])
     render json: user, include: :items
   end
 
+  # private
+  # def render_not_found_response
+  #   render json: { error: "Item not found" }, status: :not_found
+  # end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
-  resources :items, only: [:index]
-  resources :users, only: [:show]
+  resources :users, only: [:show] do
+    # nested resource for items
+    resources :items, only: %i[index create show]
+  end
+  resources :items, only: %i[index create show]
 end
